19a99af2882b761516bba2a6f26ad27ecc796ed2
Faith Robinson <57683858+Faithrobinson@users.noreply.github.com>
2019-11-13 14:16:54 -0500

    Add files via upload

---
+++ RobinsonProgram.py
@@ -0,0 +1,143 @@
+#program file
+from MargolinFunctions import*
+turtle.colormode(255)
+turtle.bgcolor(255,153,194)
+bob.speed(0)
+
+for times in range(110):
+  bob.color("hotpink")
+  bob.begin_fill()
+  bob.goto(90,-90)
+  bob.circle(times*2)
+  bob.end_fill()
+
+for times in range(105):
+  bob.color("white")
+  bob.begin_fill() 
+  bob.circle(times*2)
+  bob.end_fill()
+
+for times in range(100):
+  bob.color("hotpink")
+  bob.begin_fill() 
+  bob.circle(times*2)
+  bob.end_fill()
+
+for times in range(95):
+  bob.color("white")
+  bob.begin_fill() 
+  bob.circle(times*2)
+  bob.end_fill()
+
+for times in range(90):
+  bob.color("hotpink")
+  bob.begin_fill() 
+  bob.circle(times*2)
+  bob.end_fill()
+
+for times in range(85):
+  bob.color("white")
+  bob.begin_fill() 
+  bob.circle(times*2)
+  bob.end_fill()
+
+for times in range(80):
+  bob.color("hotpink")
+  bob.begin_fill() 
+  bob.circle(times*2)
+  bob.end_fill()
+  
+for times in range(75):
+  bob.color("white")
+  bob.begin_fill() 
+  bob.circle(times*2)
+  bob.end_fill()
+
+for times in range(70):
+  bob.color("hotpink")
+  bob.begin_fill() 
+  bob.circle(times*2)
+  bob.end_fill()
+
+for times in range(65):
+  bob.color("white")
+  bob.begin_fill() 
+  bob.circle(times*2)
+  bob.end_fill()
+
+for times in range(60):
+  bob.color("hotpink")
+  bob.begin_fill() 
+  bob.circle(times*2)
+  bob.end_fill()
+
+for times in range(55):
+  bob.color("white")
+  bob.begin_fill()
+  bob.circle(times*2)
+  bob.end_fill()
+
+for times in range(50):
+  bob.color("hotpink")
+  bob.begin_fill()
+  bob.circle(times*2)
+  bob.end_fill()
+
+for times in range(45):
+  bob.color("white")
+  bob.begin_fill()
+  bob.circle(times*2)
+  bob.end_fill()
+
+for times in range(40):
+  bob.color("hotpink")
+  bob.begin_fill()
+  bob.circle(times*2)
+  bob.end_fill()
+
+for times in range(35):
+  bob.color("white")
+  bob.begin_fill()
+  bob.circle(times*2)
+  bob.end_fill()
+
+for times in range(30):
+  bob.color("hotpink")
+  bob.begin_fill()
+  bob.circle(times*2)
+  bob.end_fill()
+
+for times in range(25):
+  bob.color("white")
+  bob.begin_fill()
+  bob.circle(times*2)
+  bob.end_fill()
+
+for times in range(20):
+  bob.color("hotpink")
+  bob.begin_fill()
+  bob.circle(times*2)
+  bob.end_fill()
+
+for times in range(15):
+  bob.color("white")
+  bob.begin_fill()
+  bob.circle(times*2)
+  bob.end_fill() 
+
+for times in range(10):
+  bob.color("hotpink")
+  bob.begin_fill()
+  bob.circle(times*2)
+  bob.end_fill() 
+
+for times in range(5):
+  bob.color("white")
+  bob.begin_fill()
+  bob.circle(times*2)
+  bob.end_fill()
+
+
+
+
+
